626ea1733d9305aa76102f56ddc497f055bfd2d3
Lucas Henrique Silva <159385729+lucashensil@users.noreply.github.com>
2024-03-06 09:57:55 -0300

    criacao da tabela_racas

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ conn = sqlite3.connect('rpg.db')
 
 cursor = conn.cursor()
 
+cursor.execute(''' CREATE TABLE tabela_racas
+                    (id_raca INTEGER PRIMARY KEY, nome_raca VARCHAR(50), descricao TEXT, alinhamento TEXT, medidas TEXT, hab_raciais TEXT) ''')
 
 
+
+
+
+
+    
 
 conn.commit()
 conn.close()
